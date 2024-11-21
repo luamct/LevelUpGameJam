dab1d1e700525ff9a364fa20b1032eacfa294e0d
xaleh <alexsilreis@gmail.com>
2024-11-21 01:13:00 -0300

    Inicio logica contratação em slots vazios apenas

--- scenes/areas/buying_spot.gd
+++ scenes/areas/buying_spot.gd
@@ -9,14 +9,12 @@ func try_to_add_adventurer(adventurer: Adventurer, slot_number: int) -> bool:
 		print("Slot is already occupied!")
 		return false
 
-	# Check if the player has enough gold to hire
-	if Globals.current_gold < buy_cost:
-		print("Not enough gold to hire!")
-		return false
-
-	Globals.current_gold -= buy_cost
-	Globals.gold_updated.emit(Globals.current_gold)
+	# Try to remove this adventurer from current slot.
+	if adventurer.spot:
+		if not adventurer.spot.try_to_remove_adventurer(adventurer):
+			return false
 	
+	try_to_hire_adventurer()
 	adventurer.spot = self
 	adventurers[slot_number] = adventurer
 
@@ -33,4 +31,24 @@ func try_to_remove_adventurer(adventurer: Adventurer) -> bool:
 	adventurers[slot_number] = null
 	adventurer_removed.emit(adventurer, slot_number)
 	print("Adventurer removed from slot:", slot_number)
+	return true
+
+func try_to_hire_adventurer() -> bool:
+	# Check if the player has enough gold to hire
+	if Globals.current_gold < buy_cost:
+		print("Not enough gold to hire!")
+		return false
+	
+	var has_free_slot = 3
+	for adventurer in adventurers:
+		var slot_number = adventurers.find(adventurer)
+		print("Slot Number: " + str(slot_number))
+		has_free_slot -= 1
+		print("Free Slot Number: " + str(has_free_slot))
+	if not has_free_slot:
+		print("There is no free slot to hire a new adventure!")
+		return false
+
+	Globals.current_gold -= buy_cost
+	Globals.gold_updated.emit(Globals.current_gold)
 	return true
