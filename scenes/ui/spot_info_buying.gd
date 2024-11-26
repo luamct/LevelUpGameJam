ad54c0b590b90b842ebe05b084810aee519ff768
xaleh <alexsilreis@gmail.com>
2024-11-26 17:24:08 -0300

    Colocando os valores dos status dos aventureiros na taverna

--- scenes/ui/spot_info_buying.gd
+++ scenes/ui/spot_info_buying.gd
@@ -8,6 +8,16 @@ const AVAILABLE_ADVENTURER_SCENE = preload("res://scenes/ui/available_adventurer
 @onready var slots_container: HBoxContainer = %SlotsContainer
 @onready var predefined_list_container: VBoxContainer = %PredefinedListContainer 
 @onready var adventurer_slots = %AdventurerSlots
+@onready var level_value = $InfoAdventurer/GridContainer/LevelContainer/Value
+@onready var strength_value = $InfoAdventurer/GridContainer/StrengthContainer/Value
+@onready var speed_value = $InfoAdventurer/GridContainer/SpeedContainer/Value
+@onready var agility_value = $InfoAdventurer/GridContainer/AgilityContainer/Value
+@onready var defense_value = $InfoAdventurer/GridContainer/DefenseContainer/Value
+@onready var attack_value = $InfoAdventurer/GridContainer/AttackContainer/Value
+@onready var morale_value = $InfoAdventurer/GridContainer/MoraleContainer/Value
+@onready var discipline_value = $InfoAdventurer/GridContainer/DisciplineContainer/Value
+@onready var status_label = $InfoAdventurer/HBoxContainer/Label
+@onready var hire_cost = $InfoAdventurer/HBoxContainer/HireCost
 
 
 func setup(spot: Spot):
@@ -26,9 +36,20 @@ func setup(spot: Spot):
 			adventurer_slots.add_child(slot)
 			slot.setup(adventurer)
 			slot.button.pressed.connect(func(): on_portrait_clicked(adventurer))
+			
 
 func on_portrait_clicked(adventurer: Adventurer):
-	print("Aventureiro clicado")
+	# Fill the adventurer status
+	status_label.text = adventurer.name + " Status -"
+	hire_cost.text = "Cost: " + str(adventurer.hire_cost) 
+	level_value.text = str(adventurer.level)
+	strength_value.text = str(adventurer.strength)
+	speed_value.text = str(adventurer.speed)
+	agility_value.text = str(adventurer.agility)
+	defense_value.text = str(adventurer.defense)
+	attack_value.text = str(adventurer.attack)
+	morale_value.text = str(adventurer.morale)
+	discipline_value.text = str(adventurer.discipline)
 
 func free_children(node: Control):
 	for child in node.get_children():
